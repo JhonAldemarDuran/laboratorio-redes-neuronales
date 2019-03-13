3be9ac0da03dabe3c74e7c7692d8e7a7dc7fab7c
JhonAldemarDuran <jhondis18@gmail.com>
2019-03-13 11:11:43 -0500

    corregir imagenes plantas

--- resources/views/planta/index.blade.php
+++ resources/views/planta/index.blade.php
@@ -7,10 +7,10 @@
     <div class="row justify-content-betwen">
         @foreach($imagens as $image)
             
-            <div class="col-4">
-                <img class="img-fluid" src="images/plantas/{{$image->name}}" alt="plantas" style="margin-bottom:2em;">
-            </div>
-
+        
+        <div class="col-4">
+                <img class="img-fluid" src="{{asset('images/plantas/'.$image->name)}}" alt="plantas" style="margin-bottom:2em;">
+        </div>
         @endforeach
         
     </div>
